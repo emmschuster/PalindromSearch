1a24f923dc4c89129fbbee9989968bf737e94bf3
emmschuster <emmschuster@tsn.at>
2020-02-20 10:58:18 +0100

    inizial commit

---
+++ src/PalClass.java
@@ -0,0 +1,13 @@
+public class PalClass {
+
+	public static void main(String[] args) {
+		String x="13022015";
+		palSuche(x);
+	}
+
+	private static void palSuche(String x) {
+		int xx=Integer.parseInt(x);
+		System.out.println(xx);
+	}
+
+}
