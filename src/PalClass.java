1a23b899a272276df400d38983a6eebf0c924d7c
emmschuster <emmschuster@tsn.at>
2020-02-20 11:36:35 +0100

    Methode done without Rek... need to do it with Rek soon

--- src/PalClass.java
+++ src/PalClass.java
@@ -1,13 +1,36 @@
+import java.time.LocalDate;
+
 public class PalClass {
 
 	public static void main(String[] args) {
-		String x="13022015";
-		palSuche(x);
+		String y="Otto";
+		y.toLowerCase();
+		String z="Peter";
+		System.out.println(palSuche(y));
+		System.out.println(palSuche(z));
+		palSuche(z);
+		LocalDate today=LocalDate.now();
+		System.out.println(today);
 	}
 
-	private static void palSuche(String x) {
-		int xx=Integer.parseInt(x);
-		System.out.println(xx);
+	private static boolean palSuche(String t) {	//ohne Rekursion
+		String c = t.replaceAll("\\s+", "").toLowerCase();
+	    int l = c.length();
+	    int f = 0;
+	    int b = l - 1;
+	    while (b > f) {
+	        char forwardChar = c.charAt(f++);
+	        char backwardChar = c.charAt(b--);
+	        if (forwardChar != backwardChar)
+	            return false;
+	    }
+	    return true;
+//		int xx=Integer.parseInt(x);
+//		System.out.println(xx);
 	}
-
+	
+//	private static boolean palSucheRek(String t) {	//Rekursion rip
+//		String c=t.replaceAll("\\s+", "");
+//		return false;	
+//	}
 }
