304b3366b9d23036f6318291019f79bf88522f68
emmschuster <emmschuster@tsn.at>
2020-02-22 12:54:28 +0100

    you can now check dates too

--- src/PalClass.java
+++ src/PalClass.java
@@ -1,36 +1,56 @@
 import java.time.LocalDate;
 
+import com.sun.org.apache.xpath.internal.operations.Plus;
+
 public class PalClass {
 
 	public static void main(String[] args) {
+		String x="20200202";
 		String y="Otto";
-		y.toLowerCase();
-		String z="Peter";
-		System.out.println(palSuche(y));
-		System.out.println(palSuche(z));
-		palSuche(z);
+		String z="Ebene";
 		LocalDate today=LocalDate.now();
-		System.out.println(today);
+		System.out.println("heute ist der "+today);
+		System.out.println(x+" -> "+palSuche(x));
+		System.out.println(y+" -> "+palSuche(y));
+		System.out.println(z+" -> "+palSuche(z));
+		LocalDate ld = LocalDate.EPOCH;
+		System.out.println("willkürliches datum : "+ld.toString());
+		String datePast="19000101";
+//		palSucheEwig(datePast);
+		
+		
 	}
 
 	private static boolean palSuche(String t) {	//ohne Rekursion
-		String c = t.replaceAll("\\s+", "").toLowerCase();
-	    int l = c.length();
+		String c = t.toLowerCase();
+	    int l = c.length()-1;
 	    int f = 0;
-	    int b = l - 1;
-	    while (b > f) {
+	    while (l > f) {
 	        char forwardChar = c.charAt(f++);
-	        char backwardChar = c.charAt(b--);
+	        char backwardChar = c.charAt(l--);
+	        System.out.println(forwardChar);
+	        System.out.println(backwardChar);
 	        if (forwardChar != backwardChar)
 	            return false;
 	    }
 	    return true;
-//		int xx=Integer.parseInt(x);
-//		System.out.println(xx);
 	}
 	
-//	private static boolean palSucheRek(String t) {	//Rekursion rip
-//		String c=t.replaceAll("\\s+", "");
-//		return false;	
-//	}
+	private static void palSucheEwig(String date) {
+		do {
+			String c = date.toLowerCase();
+		    int l = c.length()-1;
+		    int f = 0;
+		    while (l > f) {
+		        char forwardChar = c.charAt(f++);
+		        char backwardChar = c.charAt(l--);
+		        System.out.println(forwardChar);
+		        System.out.println(backwardChar);
+		        if (forwardChar != backwardChar)
+		            break;
+		    }
+		    System.out.println(date);
+		    
+		} while (date != "2100-12-31");
+	}
 }
