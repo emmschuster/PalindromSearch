494ae6cfe6f328546691da83b565091e3dfcf840
emmschuster <emmschuster@tsn.at>
2020-02-26 19:56:54 +0100

    failed to to the specific date task

--- src/PalClass.java
+++ src/PalClass.java
@@ -16,9 +16,32 @@ public class PalClass {
 		LocalDate ld = LocalDate.EPOCH;
 		System.out.println("willkürliches datum : "+ld.toString());
 		String datePast="19000101";
+//		fragen wie des gemacht werden soll
 //		palSucheEwig(datePast);
 		
+//		rip irgednwie... heeelp
+//		palSuche(x,1);	
 		
+	}
+
+	private static boolean palSuche(String x, int i) {
+		if(x.length()>=9) {
+			System.out.println("Das Datum darf nur 8 Zahlen lang sein!!!");
+		} else if (i==1) {
+			String c = x.toLowerCase();
+		    int l = c.length()-1;
+		    int f = 0;
+		    while (l > f) {
+		        char forwardChar = c.charAt(f++);
+		        char backwardChar = c.charAt(l--);
+		        System.out.println(forwardChar);
+		        System.out.println(backwardChar);
+		        if (forwardChar != backwardChar)
+		            return false;
+		    }
+		    return true;
+		}
+		return false;
 	}
 
 	private static boolean palSuche(String t) {	//ohne Rekursion
